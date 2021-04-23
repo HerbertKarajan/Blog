f4d097d29ba7a182a7fed4c325d6f0d1fac147f2
wangyongjiang6 <wangyongjiang6@gome.com.cn>
2021-04-23 22:19:44 +0800

    更新配置

--- deploy.sh
+++ deploy.sh
@@ -11,6 +11,7 @@ cd docs/.vuepress/dist
 
 # 如果是发布到自定义域名
 # echo 'www.example.com' > CNAME
+
 git init
 git add -A
 git commit -m 'deploy'
@@ -19,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
 git push -f git@github.com:HerbertKarajan/Blog.git master:gh-pages
+
 cd -
