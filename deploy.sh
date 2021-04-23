489555245e7ff7fa08aa805547d661e58b577ca1
wangyongjiang6 <wangyongjiang6@gome.com.cn>
2021-04-23 09:38:45 +0800

    开设博客

--- deploy.sh
+++ deploy.sh
@@ -15,10 +15,10 @@ git init
 git add -A
 git commit -m 'deploy'
 
-# 如果发布到 https://<USERNAME>.github.io  USERNAME=你的用户名
-# git push -f git@github.com:USERNAME/REPO.github.io.git master
+# 如果发布到 https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
-# 如果发布到 https://<USERNAME>.github.io/<REPO>  REPO=github上的项目
-git push -f git@github.com:HerbertKarajan/Blog.git main:gh-pages
-
+# 如果发布到 https://<USERNAME>.github.io/<REPO>
+# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+git push -f git@github.com:HerbertKarajan/Blog.git master:gh-pages
 cd -
