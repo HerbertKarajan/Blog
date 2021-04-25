6d01fa204ecdaff5ac2b173eeb993acc135abacd
wangyongjiang6 <wangyongjiang6@gome.com.cn>
2021-04-25 10:08:07 +0800

    开设博客

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:HerbertKarajan/Blog.git main:gh-pages
+git push -f git@github.com:HerbertKarajan/Blog.git master:gh-pages
 
 cd -
